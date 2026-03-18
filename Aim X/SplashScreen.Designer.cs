17494030044dcc495dcf64738998ce0fd5e09a56
MR.PC GAMER <ayanr8096@gmail.com>
2026-03-18 10:51:47 +0530

    Update SplashScreen.Designer.cs

--- Aim X/SplashScreen.Designer.cs
+++ Aim X/SplashScreen.Designer.cs
@@ -59,7 +59,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Aim X";
             this.ResumeLayout(false);
 
         }
